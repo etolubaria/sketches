dbdff3ce54437c27409801ed7013fac4cc4bec5c
etolubaria <m203a4@gmail.com>
2019-12-10 17:41:04 +0200

    Create EmailAddress.php

---
+++ value-objects/EmailAddress.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Email address value object
+ *
+ * @author Eugene Tolubaria <m203a4@gmail.com>
+ */
+class EmailAddress
+{
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     */
+    private $name;
+
+    /**
+     * @param string $email
+     * @param string|null $name
+     */
+    public function __construct(string $email, ?string $name = null)
+    {
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        if (null === $this->name) {
+            return $this->email;
+        }
+
+        return sprintf('%s<%s>', $this->name, $this->email);
+    }
+}
