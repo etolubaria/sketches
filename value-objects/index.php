21b2be3e661906d86d63a3ee19b6ea1ed1199958
etolubaria <m203a4@gmail.com>
2019-12-10 17:36:57 +0200

    Create index.php

---
+++ value-objects/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$address1 = new EmailAddress('m203a4@gmail.com');
+$address2 = new EmailAddress('m203a4@gmail.com', 'Eugene Tolubaria');
+
+echo $address1, PHP_EOL;
+echo $address2, PHP_EOL;
