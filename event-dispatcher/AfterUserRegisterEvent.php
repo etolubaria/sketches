a97819b5eec87c2ba69900cb880aee20045d16aa
etolubaria <m203a4@gmail.com>
2019-12-10 17:02:15 +0200

    Create AfterUserRegisterEvent.php

---
+++ event-dispatcher/AfterUserRegisterEvent.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Eugene Tolubaria <m203a4@gmail.com>
+ */
+class AfterUserRegisterEvent
+{
+    /**
+     * @var User
+     */
+    private $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
