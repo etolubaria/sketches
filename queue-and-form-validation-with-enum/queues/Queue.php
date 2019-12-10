1466a5329f844213b4291d30514ff79b4ebf1139
etolubaria <m203a4@gmail.com>
2019-12-10 17:57:22 +0200

    Create Queue.php

---
+++ queue-and-form-validation-with-enum/queues/Queue.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\queues;
+
+/**
+ * @author Eugene Tolubaria <m203a4@gmail.com>
+ */
+class Queue
+{
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @param array $payload
+     */
+    public function push(array $payload): void
+    {
+        array_push($this->data, $payload);
+    }
+
+    /**
+     * @return array
+     */
+    public function pop(): array
+    {
+        return array_pop($this->data);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEmpty(): bool
+    {
+        return count($this->data) === 0;
+    }
+
+    /**
+     * Dumps internal queue data
+     */
+    public function dump(): void
+    {
+        var_dump($this->data);
+    }
+}
