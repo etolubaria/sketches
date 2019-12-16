243252227c5311ed86c66819ae412a050b254720
etolubaria <m203a4@gmail.com>
2019-12-16 20:13:19 +0200

    Update AbstractProcessor.php

--- user-info-processing/App/Module/UserInfo/Processor/AbstractProcessor.php
+++ user-info-processing/App/Module/UserInfo/Processor/AbstractProcessor.php
@@ -1,12 +1,38 @@
 <?php
 
+/*
+ * This is a test application.
+ *
+ * It shows an example of working with an external API
+ *   and processing request-response in various formats.
+ *
+ * Should be ran from the PHP Command Line Interface.
+ *
+ * (c) Eugene Tolubaria <m203a4@gmail.com>
+ */
+
 namespace App\Module\UserInfo\Processor;
+
+use Contract\HttpClient\HttpClientInterface;
 
 /**
  * @author Eugene Tolubaria <m203a4@gmail.com>
  */
 abstract class AbstractProcessor implements ProcessorInterface
 {
+    /**
+     * @var HttpClientInterface
+     */
+    private $httpClient;
+
+    /**
+     * @param HttpClientInterface $httpClient
+     */
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
     /**
      * @param array $userInfo
      * 
@@ -28,8 +54,7 @@ abstract class AbstractProcessor implements ProcessorInterface
     {
         $request = $this->prepareRequest($userInfo);
 
-        $httpClient = new HttpClient();
-        $response = $httpClient->send('GET', 'someWhere', $request);
+        $response = $this->httpClient->send('GET', 'someWhere', $request);
 
         return $this->getStatusFromResponse($response);
     }
