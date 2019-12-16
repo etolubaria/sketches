9c4191610ae196b693ab001af0b959095097c926
etolubaria <m203a4@gmail.com>
2019-12-16 19:39:58 +0200

    Create AbstractProcessor.php

---
+++ user-info-processing/App/Module/UserInfo/Processor/AbstractProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Module\UserInfo\Processor;
+
+/**
+ * @author Eugene Tolubaria <m203a4@gmail.com>
+ */
+abstract class AbstractProcessor implements ProcessorInterface
+{
+    /**
+     * @param array $userInfo
+     * 
+     * @return string Prepared HTTP request
+     */
+    abstract protected function prepareRequest(array $userInfo): string;
+
+    /**
+     * @param string $userInfo
+     * 
+     * @return string Response status
+     */
+    abstract protected function getStatusFromResponse(string $response): string;
+
+    /**
+     * @inheritDoc
+     */
+    public function process(array $userInfo): string
+    {
+        $request = $this->prepareRequest($userInfo);
+
+        $httpClient = new HttpClient();
+        $response = $httpClient->send('GET', 'someWhere', $request);
+
+        return $this->getStatusFromResponse($response);
+    }
+}
