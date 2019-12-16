2f0e342bb9819732f965a9a8830580564531ede8
etolubaria <m203a4@gmail.com>
2019-12-16 19:32:15 +0200

    Create FormatType.php

---
+++ user-info-processing/App/Module/UserInfo/Enum/FormatType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Module\UserInfo\Enum;
+
+/**
+ * @author Eugene Tolubaria <m203a4@gmail.com>
+ */
+final class FormatType
+{
+    public const JSON = 'json';
+    public const XML = 'xml';
+}
