16f9cccc779a22dc94f529ce898795ada8e0ac8b
etolubaria <m203a4@gmail.com>
2019-12-16 20:06:41 +0200

    Update HttpClientInterface.php

--- user-info-processing/Contract/HttpClient/HttpClientInterface.php
+++ user-info-processing/Contract/HttpClient/HttpClientInterface.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This is a test application.
+ *
+ * It shows an example of working with an external API
+ *   and processing request-response in various formats.
+ *
+ * Should be ran from the PHP Command Line Interface.
+ *
+ * (c) Eugene Tolubaria <m203a4@gmail.com>
+ */
+
 namespace Contract\HttpClient;
 
 /**
